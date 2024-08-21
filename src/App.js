0b5a3f33e22935ffbd3178d2c9b4aa03028d3db5
eflaatten <elliotflaatten@live.com>
2024-08-21 00:45:12 -0500

    componentDidMount fetch front page posts. map listarticles on page. CSS styling.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,40 @@ import SearchForm from './components/SearchForm';
 import DisplayArticleCard from './components/DisplayArticleCard';
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-
-    }
+      listOfArticles: [],
+    };
   }
 
-  render(){
+  // front page loads on inital render
+  componentDidMount() {
+    fetch('http://hn.algolia.com/api/v1/search?tags=front_page')
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        listOfArticles: data.hits,
+      });
+    });
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-  
-        </header>
+      <div className='App'>
+          <h1>Hacker News</h1>
+          {this.state.listOfArticles.map((article, index) => (
+            <ListArticles
+              key={index}
+              title={article.title}
+              author={article.author}
+              url={article.url}
+              points={article.points}
+              num_comments={article.num_comments}
+              created_at={article.created_at}
+            />
+          ))}
       </div>
     );
   }
