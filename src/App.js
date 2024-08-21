da2789663ad7128677af037286553041063c2587
eflaatten <elliotflaatten@live.com>
2024-08-20 20:51:21 -0500

    init component files and App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import ListArticles from './components/ListArticles';
+import SearchForm from './components/SearchForm';
+import DisplayArticleCard from './components/DisplayArticleCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+  
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ListArticles.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ListArticles = (props) => {
+
+}
+
+export default ListArticles;
