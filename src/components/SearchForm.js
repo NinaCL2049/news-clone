43638f048f7c03992a7b0433b7570f4a58f9826d
eflaatten <elliotflaatten@live.com>
2024-08-23 12:38:27 -0500

    changed SearchForm to select dropdowns. can somewhat filter through posts.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,33 +3,39 @@ import React, { useState } from 'react';
 
 const SearchForm = (props) => {
 
-  const [tag, setTag] = useState('');
-  const [date, setDate] = useState('');
-  const [author, setAuthor] = useState('');
-  const [title, setTitle] = useState('');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.onSearch({ tag, date, author, title });
-  };
+  const { forSearch, bySearch, timeSearch, handleTypeSearch, handleTimeSearch } = props;
 
   return (
-    <form className="search-form" onSubmit={handleSubmit}>
-      <div>
-        <input type="text" placeholder="Tag" value={tag} onChange={(e) => setTag(e.target.value)} />
-      </div>
-      <div>
-        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-      </div>
-      <div>
-        <input type="text" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-      </div>
-      <div>
-        <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </div>
-      <button type="submit">Search</button>
-    </form>
-  );
+    <div className="search-form">
+      <label>Search</label>
+      <select placeholder="Select" className="for-search targeted-search" onChange={handleTypeSearch}>
+        <option value="all">All</option>
+        <option value="stories">Stories</option>
+        <option value="comments">Comments</option>
+        <option value="ask-hn">Ask HN</option>
+        <option value="show-hn">Show HN</option>
+        <option value="jobs">Jobs</option>
+        <option value="polls">Polls</option>
+      </select>
+
+      <label>By</label>
+      <select placeholder="Select" className="by-search targeted-search">
+        <option value="popularity">Popularity</option>
+        <option value="date">Date</option>
+      </select>
+
+      <label>For</label>
+      <select placeholder="Select" className="time-search targeted-search" onChange={handleTimeSearch}>
+        <option value="all-time">All Time</option>
+        <option value="last-24h">Last 24h</option>
+        <option value="past-week">Past Week</option>
+        <option value="past-month">Past Month</option>
+        <option value="past-year">Past Year</option>
+      </select>
+
+    </div>
+  )
+
 };
 
 export default SearchForm;
